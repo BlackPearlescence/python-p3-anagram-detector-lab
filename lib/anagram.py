69a83faa533cc7e0115a7bdc3281e0c735ef7457
Sani Rogers <sanirogers138@gmail.com>
2023-02-21 14:02:38 -0500

    do anagram:

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self._word = word
+    def match(self,word_list):
+        myword = [*self._word]
+        myword.sort()
+        matches = []
+        for i in range(len(word_list)):
+            compword = [*word_list[i]]
+            compword.sort()
+            if(myword == compword):
+                matches.append(word_list[i])
+        return matches
+
+ana = Anagram("enlist")
+matches = ana.match(["listen", "silent", "hippopotamus"])
+print(matches)
